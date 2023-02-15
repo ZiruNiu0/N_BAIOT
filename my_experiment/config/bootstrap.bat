a4a1521280613616252c997fafbf30cae088cc0a
Chaoyang He <chaoyanghe.com@gmail.com>
2023-02-14 20:59:05 -0800

    bug fix for boostrap.sh

--- my_experiment/config/bootstrap.bat
+++ my_experiment/config/bootstrap.bat
@@ -1,11 +1,8 @@
 :: ### don't modify this part ###
 :: ##############################
-
-
-:: ### please customize your script in this region ####
-set DATA_PATH=%userprofile%\fedml_data
-if exist %DATA_PATH% (echo Exist %DATA_PATH%) else mkdir %DATA_PATH%
-
+pip install -U fedml
+pip install scikit-learn
+pip install pandas
 
 :: ### don't modify this part ###
 echo [FedML]Bootstrap Finished
--- my_experiment/config/bootstrap.sh
+++ my_experiment/config/bootstrap.sh
@@ -1,4 +1,6 @@
 pip install --upgrade fedml
+pip install scikit-learn
+pip install pandas
 
 ### don't modify this part ###
 echo "[FedML]Bootstrap Finished"
